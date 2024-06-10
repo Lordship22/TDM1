ab56759deed5bdaec6afc3dff27623c1f71f1d59
Lordship22 <149322134+Lordship22@users.noreply.github.com>
2024-06-10 17:57:01 +0300

    Create lvl.js

---
+++ plugins/lvl.js
@@ -0,0 +1,48 @@
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { levelup } from '../lib/canvas.js'
+
+let handler = async (m, { conn }) => {
+	let name = conn.getName(m.sender)
+    let user = global.db.data.users[m.sender]
+    if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+        let { min, xp, max } = xpRange(user.level, global.multiplier)
+        throw `
+*╮───⊷ المستوي*
+▢ الاسم : *${name}*
+▢ المستوي : *${user.level}*
+▢ الخبرة : *${user.exp - min}/${xp}*
+▢ اللقب الحالي : _*${user.role}*_
+*╯─────────────⊷*
+
+انت تحتاج الي *${max - user.exp}* *XP* 
+لرفع مستواك ي وحش 🤩 هل يود؟
+`.trim()
+    }
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+    if (before !== user.level) {
+        let teks = `🎊 عاااااااااااااش  ${conn.getName(m.sender)}    المستوي:`
+        let str = `
+*╮──⊷ المستوى ⊶*
+▢ المستوي السابق : *${before}*
+▢ المستوي الحالي : *${user.level}*
+▢ اللقب الجديد : _*${user.role}*_
+*╯──────────────⊶*
+
+*_كلما تفاعلت معي كلما ارتفع مستوا خاصتك_*
+`.trim()
+        try {
+            const img = await levelup(teks, user.level)
+            conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
+        } catch (e) {
+            m.reply(str)
+        }
+    }
+}
+
+handler.help = ['levelup']
+handler.tags = ['xp']
+
+handler.command = ['لفل', 'lvl', 'levelup', 'مستواي', 'مستوا'] 
+
+export default handler
