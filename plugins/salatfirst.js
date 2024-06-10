a8888f9cb2e93730f9bb103a4eb02b41a5eac430
Lordship22 <149322134+Lordship22@users.noreply.github.com>
2024-06-10 17:18:38 +0300

    Update salatfirst.js

--- plugins/salatfirst.js
+++ plugins/salatfirst.js
@@ -15,7 +15,7 @@ export async function before(m) {
         العشاء: "20:33"
     };
     const date = new Date(new Date().toLocaleString("en-US", {
-      timeZone:"Asia/Makkah"
+      timeZone:"Africa/Casablanca"
     }));
     const hours = date.getHours();
     const minutes = date.getMinutes();
