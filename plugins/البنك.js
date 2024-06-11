2911fc997feac4b15dc4c4b329ec74aee7c9fcaa
Lordship22 <149322134+Lordship22@users.noreply.github.com>
2024-06-11 14:28:46 +0300

    Update البنك.js

--- plugins/البنك.js
+++ plugins/البنك.js
@@ -2,8 +2,32 @@ let handler = async (m, {conn, usedPrefix}) => {
 	
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
-    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-    conn.reply(m.chat, `
+    let username = conn.getName(who)
+    //let { wealth } = global.db.data.users[who]
+    if (!(who in global.db.data.users)) throw `✳️ المستخدم غير مسجل في قاعدة بياناتي`
+
+    var wealth = 'مسكين عالحديده😭'
+     if (`${user.money}`           <= 3000){
+            wealth = 'مسكين عالحديده😭'
+      } else if (`${user.money}`   <= 60000){
+            wealth = 'مطفر😢'
+        } else if (`${user.money}` <= 350000){
+            wealth = 'متوسط💸'
+        } else if (`${user.money}` <= 8500000){
+            wealth = 'مقروش💸💰'
+        } else if (`${user.money}` <= 1500000){
+            wealth = 'مليونير🤑'
+        } else if (`${user.money}` <= 3000000){
+            wealth = 'مليونير-متعدد🤑'
+        } else if (`${user.money}` <= 10000000){
+            wealth = 'ملياردير🤑🤑'
+        }    
+    
+    conn.reply(m.chat, `🏦 *Bank | ${username}*
+
+*▢💮 بيلي* : ${user.money}
+*اجمالي الثروة:* ${wealth}
+
 ╮───⊷ *الرصـــيد* ⊶
 ▢ *📌الإسم* : _@${who.split('@')[0]}_
 ▢ *💎الماس* : _${user.diamond}_
@@ -15,17 +39,18 @@ let handler = async (m, {conn, usedPrefix}) => {
 ▢ *🪵الخشب* : _${user.wood}_
 ▢ *🥤الجرعات* : _${user.potion}_
 ▢ *🔩حديد* : _${user.iron}_
-▢ *💮البيلي* : _${user.money}_
-▢ *🛡️المجد/خبرة* : _${user.exp}_
+▢ *🛡️المــجــــد/خبرة* : _${user.exp}_
 ╯──────────────⊶
 
 *‼️ ملاحظه :* 
 *يمكنك شراء 💎 الماس باستخدام الأوامر*
 ❏ *${usedPrefix}شراء* <. . .>
-❏ *${usedPrefix}اشتر-الكل*`, m, { mentions: [who] })
+❏ *${usedPrefix}اشتر-الكل*
+
+`, m, { mentions: [who] })  //${user.chicken}
 }
-handler.help = ['balance']
-handler.tags = ['econ']
-handler.command = ['bal', 'البنك', 'diamond', 'balance'] 
+handler.help = ['bank']
+handler.tags = ['economy']
+handler.command = ['bank', 'vault','بنك','البنك'] 
 
 export default handler
